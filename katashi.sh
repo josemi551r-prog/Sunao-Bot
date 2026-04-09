441ea2881d1aa1ea680bc1592b9d25d920da3406
josemi551r-prog <josemi551r@gmail.com>
2026-04-08 18:46:42 -0600

    Update Sunao.sh

--- katashi.sh
+++ katashi.sh
@@ -1,4 +1,4 @@
-# Código creado por @gata_dios      
+      
 
 #!/data/data/com.termux/files/usr/bin/bash 
 # Interpretación determinada para la ejecución      
@@ -202,7 +202,7 @@ echo -e "\e[35m
 ╚═╝░░░░░╚═╝╚═════╝░\n\e[0m"
 
 echo -e "\033[1;35m"
-git clone https://github.com/KatashiFukushima/KatashiBot-MD.git
+git clone https://github.com/josemi551r-prog/Sunao-Bot.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository directory!!\n\033[0m" 
